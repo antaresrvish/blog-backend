430881201e208e682e1d83e9f12bee25b60c3d6e
antaresrvish <yildirimyusuf758@gmail.com>
2024-10-07 16:54:03 +0300

    base64 encoding fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import db from './src/db/index.js';
 import { postSchema, commentSchema } from './src/schemas/schema.js'
 import { eq } from 'drizzle-orm';
-import { ExecuteResultSync } from 'drizzle-orm/sqlite-core';
+import multer from 'multer';
 const app = express();
 const port = 3000;
 
@@ -18,8 +18,9 @@ app.get('/posts', async (req, res) => {
         const Mposts = posts.map(post => {
             let imageBlobUrl = null;
             if(post.imageBlobUrl) {
-                const base64Data = `data:image/png;base64,${post.imageBlobUrl}`;
-                const buffer = Buffer.from(base64Data.split(',')[1], 'base64');
+                const base64Data = `${post.imageBlobUrl}`;
+                console.log(base64Data);
+                const buffer = Buffer.from(base64Data, 'base64');
                 console.log(buffer);
                 const blob = new Blob([buffer], { type: 'image/png' });
                 console.log(blob);
@@ -37,16 +38,33 @@ app.get('/posts', async (req, res) => {
         res.status(500).send('SERVER ERROR')
     }
 });
+const upload = multer({ storage: multer.memoryStorage() });
 
-app.post('/posts', async (req, res) => {
+const blobToBase64 = async (blob) => {
+    const arrayBuffer = await blob.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    return buffer.toString('base64');
+};
+app.post('/posts',upload.fields([
+    { name: 'imageBlobUrl', maxCount: 1 }
+  ]), async (req, res) => {
     try{
+
+        const imageFile = req.files['imageBlobUrl'];
+        console.log(imageFile);
+        const blob = new Blob([imageFile[0].buffer], {type: imageFile[0].mimetype});
+        console.log(blob);
+        const base64 = await blobToBase64(blob);
+        const url = `data:${imageFile[0].mimetype};base64,${base64}`
+        console.log(url);
+
         await db.insert(postSchema).values({
             title: req.body.title,
             description: req.body.description,
             content: req.body.content,
             tags: req.body.tags,
             author: req.body.author,
-            imageBlobUrl: req.body.imageBlobUrl,
+            imageBlobUrl: url
         });
         res.status(200).send('OK');
     } catch(ex) {
